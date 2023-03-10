53fc41a9a3df2a0e3d41f3e9d877d0a22d4c4a5a
Radu Oprescu <125269530+OprescuRadu@users.noreply.github.com>
2023-03-10 18:20:13 +0200

    Time: 19 ms (41.78%), Space: 55.8 MB (48.40%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+                set.add(nums[i]);
+        }
+        if(nums.length != set.size()){
+            return true;
+        }
+        return false;
+    }
+}
